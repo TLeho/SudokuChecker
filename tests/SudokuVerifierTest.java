485600d784245f004228b0023197f18df48dba17
TLeho <93613259+TLeho@users.noreply.github.com>
2021-11-26 20:54:57 +0200

    Add files via upload
    Added new tests

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -10,6 +10,17 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 String c = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 String i = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+
+// added for the task
+String test1 = "";
+String test2 = "417698563258947587231685431699158643236912758289643571573291684164875293";
+String test3 = "417369825632158947958724316825437169791586432346912758289643571573291684164875293123456789";
+String test4 = "447369825632158947958724316825437169791586432346912758289643571573291684164875293";
+String test5 = "143456789256789123789123456471362598892415367365897214534648971617932845928571632";
+String test6 = "123456789456789123789123456471632598892514367365897214537961842612348975948275631";
+String test7 = "1£3456789912345678891234567789123456678912345567891234456789123345678912234567891";
+String test8 = "407369825632158947958724316825437169791586432346912758289643571573291684164875293";
+
 SudokuVerifier v = new SudokuVerifier();
 
 	@Test
@@ -21,8 +32,55 @@ SudokuVerifier v = new SudokuVerifier();
 	@Test
 	public void testIncorrectString() {
 		int a = v.verify(i);
+		assertEquals("incorrect string", a, -2);	
+	}
+	
+	@Test
+	public void test_NoSudokuTables() {
+		int a = v.verify(test1);
+		assertEquals("incorrect string", a, -1);	
+	}
+	
+	@Test
+	public void test_8numbersPerTable() {
+		int a = v.verify(test2);
+		assertEquals("incorrect string", a, -1);	
+	}
+	
+	@Test
+	public void test_TenSudokuTables() {
+		int a = v.verify(test3);
+		assertEquals("incorrect string", a, -1);
+	}
+			
+	@Test
+	public void test_TwoSameNumbersInOneTable() {
+		int a = v.verify(test4);
 		assertEquals("incorrect string", a, -2);
-		
+	}
+	
+	@Test
+	public void test_TwoSameNumbersInGlobalRow() {
+		int a = v.verify(test5);
+		assertEquals("incorrect string", a, -3);
+	}
+	
+	@Test
+	public void test_TwoSameNumbersInGlobalColumn() {
+		int a = v.verify(test6);
+		assertEquals("incorrect string", a, -4);
+	}
+	
+	@Test 
+	public void test_UnicodeCharacterInSudoku() {
+		int a = v.verify(test7);
+		assertEquals("incorrect string", a, 1);
+	}
+	
+	@Test
+	public void test_ZeroInSudoku() {
+		int a = v.verify(test8);
+		assertEquals("incorrect string", a, -1);
 	}
 	
 	
